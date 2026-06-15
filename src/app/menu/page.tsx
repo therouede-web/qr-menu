e06afc2584cae49a339b4f13bf49f06d50f02aad
therouede-web <therouede@gmail.com>
2026-06-16 00:59:13 +0200

    Update page.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
 import Menu from "@/components/Menu"
 
-function page() {
-  return (
-    <Menu />
-  )
+export default function page() {
+  return <Menu />
 }
-
-export default page
